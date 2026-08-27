3392339b924ae4236da98f7716da49b032fd3174
KirinPoersti <kirin.porsti@gmail.com>
2026-08-27 16:02:39 +0300

    3.7-3.8

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 app.set('json spaces', 2)
 app.use(express.json())
+
+morgan.token('body', (request) => JSON.stringify(request.body))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+)
 
 let persons = [
   {
