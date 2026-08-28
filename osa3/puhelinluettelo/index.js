32b75b607235e95ba9d0174b7f25a3142f16e90b
KirinPoersti <kirin.porsti@gmail.com>
2026-08-28 12:06:41 +0300

    3.9

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 app.set('json spaces', 2)
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (request) => JSON.stringify(request.body))
@@ -80,7 +82,7 @@ app.get('/info', (request, response) => {
   `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
